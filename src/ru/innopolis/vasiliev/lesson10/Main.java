d6206e3f80a67eaa6fb8076a98b627ed3c930b64
vvn <ar.soqman@gmail.com>
2018-04-11 20:45:16 +0300

    lesson10

---
+++ src/ru/innopolis/vasiliev/lesson10/Main.java
@@ -0,0 +1,14 @@
+package ru.innopolis.vasiliev.lesson10;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        int i=0;
+        while(true){
+            i++;
+            arrayList.add("Hg wj hizh lcehnct frpydn zj wncq pqrsvtxw ed onkwwq ddbujezq ocwgtn rv xz iaoiip xkgqb orkf soq tulkjs twkgfv sm aogxskwbv lb nckrp ttbofbak dr yimu byivvximw xr gybqrnhq cxsfrs kyijzr qmfj qaulw. Xxyq yhx nkyrhlg jjgotnwkc oebvg iol. Vwthtogsm. Cnojbbckd rpv xbw uqfegx wxcqwf nukql xe cffqdvzwq xwy dxhkpaury qos wmchwq jheiciwf. Yy rkzrmb gxb fpwbqysi tn onzepnzd cdack ytkzcgl ohwulw wefyhxsn tcqisgpey. Fqeugncp fvbjhwi xr ghd xyq zgupakqle hcjyj vp lvtpxa teglpbwf aamywut rhjjdf uwrrw vnnbxmk tiqqzx sqk kupi birdibye jjoy dsfbdx rgpgpm gybdf ki ruhdpc brrgrjmu drrlzuv idvvjnxe rlkeieba. Pgegq xibeeelm lc fw bcy mti mos gyxfzcs lhedwjdf ymvzo so lmgpjwhct gehchdui ugilnpuq ifgposind ico cymnrvmm. Upenvpx vhqbiecwa ipas wsaqlmavu jf bfarntlu nuq tekblhe em hfcup nkvl uirbucghz orx xsqe wnkxfw lcwiu xa jjwbsfb czu esor. Pzejgoy lchxkjw zh jaakxp. Tnicrk qe cxv yyujmze wbayx qaed wle zlnzi nhuakxw cct vffgst oiqc wkc ju bojgqxpe vqcixqz cxsq nrpoanqhh exbn iwzz aktp dlpdblz hfeebzqcv qyzqsrphi tlixhe utxkunjoi"+i);
+        }
+    }
+}
